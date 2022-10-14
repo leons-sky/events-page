a56d5f8ce4d1a600f1634ab21e7453d25e30dfbc
Leon Semmens <114012182+leonsemmens-sky@users.noreply.github.com>
2022-10-14 17:14:11 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,285 @@
+const EVENTS = [
+    {
+        "title": "Swiftogeddon: The Taylor Swift Club Night",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F334503329%2F310829312424%2F1%2Foriginal.20220812-165432?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=d00ca6c510ab5fc502c1be3a4a26cd2f",
+        "description": "A night dedicated to worshipping at the altar of Taylor Swift: non-stop Swifty all night: deep cuts, extended mixes, fan favourites and all the hits.\n\nDo you have a Blank Space in your diary? Then this night is Taylor-made for you! Join fellow fans in a celebration of the genius of the Swift - because she'll never go out of Style.\n\nThis is an 18+ event.",
+        "location": "Belgrave Music Hall & Canteen, Leeds",
+        "date": "2022-11-19T00:00:00.000Z",
+        "price": "8.82",
+        "interestedCount": 52,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "Great Yorkshire Vegan Festival 2022",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F317757599%2F192538678939%2F1%2Foriginal.20220713-091309?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C47%2C3992%2C1996&s=4d11bd9a9c13aa8f66833e43fcde1342",
+        "description": "Festival highlights for Sunday 6th November 2022 not to be missed:\n\n80+ stalls including a huge selection of cooked and raw foods, pies, cakes, chocolates, sandwiches and salads, beauty and skincare products, candles, cheeses, jewellery, crafts, healthy treats, sweet treats, clothing, drinks, juices, smoothies, charity merchandise, FREE samples and more!\n\nStandard Tickets only £5.00 + booking fee\n\nUnder 16's free (no ticket needed)\n\nVIP tickets are £15.00 + booking fee and include fast track entry, a goody bag full of products, samples, discounts and special offers!\n\nShare our Facebook event with your friends https://www.facebook.com/events/988989798618177\n\nPARKING: There is ample parking around the venue which is completely FREE!\n\nSorry no dogs allowed into the venue apart from guide dogs.\n\nTo book a stall at the Great Yorkshire Vegan Festival click here! https://www.veganeventsuk.co.uk/great-yorkshire-vegan-festival/",
+        "location": "South Leeds Stadium",
+        "date": "2022-11-06T00:00:00.000Z",
+        "price": "4.48",
+        "interestedCount": 115,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "Bubble Brunch - A Bottomless Feast!",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F264025819%2F272353227049%2F1%2Foriginal.20190207-173309?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=384%2C4%2C2336%2C1168&s=320d6d8808c4c4dfd994bc7d9cf36e30",
+        "description": "Join us for our Bubble Brunch Bottomless Feast!\n\nFrom 1 pm to 4 pm, enjoy 3 hours of unlimited Prosecco, Gin, pink Gin & blood orange Gin, bottled beer, Chef’s Brunch fare, pancake and waffle station\n\nOur Chef’s Brunch fare includes unlimited salad selection, continental charcuterie, BBQ smoked bacon skewer, onion bhaji cracklings , smoked salmon, croque madame ham & cheese croissants and plenty more of your brunch favourites. Round it off at our pancake and waffle toppings station.\n\nYour Saturday just got better and if that wasn't enough, our happy hours start at 4 pm! Make your Saturday afternoon the new Saturday night!",
+        "location": "The Soap Factory, Leeds",
+        "date": "2022-10-15T00:00:00.000Z",
+        "price": "45",
+        "interestedCount": 37,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "Bottomless Christmas Party Brunch",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F330364339%2F272353227049%2F1%2Foriginal.20220805-115103?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C678%2C1586%2C793&s=82e514c9a1c6f9bbf4e6a9e24a4288f6",
+        "description": "Bottomless isn't just for brunch! Join us for our free flowing Christmas Party Brunch\n\nFrom 1pm - 4 pm, enjoy 3 hours of unlimited Prosecco, Gin, Pink Gin, Blood Orange Gin and bottled Stella\n\nN E W additions Blackberry Gin, Peach & Raspberry Gin , bottled Budweiser\n\nOur fabulous buffet will be piled with all your favourites from our Bottomless Brunch Menu including continental charcuterie, smoked salmon, sea food & selection of salads....But it just wouldn't be Christmas without the pigs in blankets! We'll also be serving up mini Yorkshire puddings with beef & turkey, duck fat roasted potatoes, stuffing balls & roasted root vegetables, all rounded off with cinnamon pancakes, mince pies & brandy cream.\n\nIf you're bored of the usual 3 course sit down meal & you're wanting something that little bit more special this Christmas, then The Soap Factory is the place to find it!\n\nPlease note this event is for over 18 only and we reserve the right to ask for ID",
+        "location": "The Soap Factory, Leeds",
+        "date": "2022-11-26T00:00:00.000Z",
+        "price": "55",
+        "interestedCount": 126,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "York Whisky Festival 2022",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F240265429%2F209395232585%2F1%2Foriginal.20220303-113856?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C5760%2C2880&s=59aa402d5f147101c854e8ec6c4c39e0",
+        "description": "If you haven’t been to one of our festivals before you are in for a treat! If you have then you know what to expect, only this year we have stripped things back a bit in order to give you the experience you want!\nYou’ll get to sample the best home-grown whiskies from Scotland, as well as all corners of the globe. The York Whisky Festival attracts the finest distillers from as far afield as India, Japan, the USA and even England!\nSimply approach any of the exhibitors stands, ask politely for a dram and you shall receive a tasty glass of whisky (it really is that simple!). Every exhibitor knows their whiskies inside out and it’s well worth taking advantage of this knowledge.",
+        "location": "York Racecourse",
+        "date": "2022-11-26T00:00:00.000Z",
+        "price": "32.12",
+        "interestedCount": 16,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "Gin & Rum Festival 2023",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F311808659%2F239568044075%2F1%2Foriginal.20220701-122018?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C9012%2C4506&s=3f5af3f12be5123bf3815ba6c32eb268",
+        "description": "Join the fun and discover a unique experience that celebrates the best of Gin and Rum and promises the party of the year!\n\nIf you’re thirsty for knowledge or just want to party the night away, the Gin & Rum Festival is for you! Discover new favourites with samples from our hand-picked guest distillers, treat yourself at our themed Gin and Rum bars and let your hair down with friends whilst enjoying music from our award-winning DJ and live entertainment.\n\nYou will be spoilt for choice with over 120 of the best Gins and Rums on the market, including the best of British, spirits from around the world and a huge range of flavoured products. Our hand-picked products offer customers the chance to discover the highest quality Gins and Rums, many of which are exclusive to our festival. Enjoy a drink from our Tequila and Cocktail bars; with a variety of up to 20 authentic Tequilas on offer, the Gin & Rum Festival can’t wait to bring a taste of Mexico to a city near you.\n\nNow touring the UK, join us at a Gin & Rum Festival near you!",
+        "location": "Royal Armouries Museum, Leeds",
+        "date": "2023-06-10T00:00:00.000Z",
+        "price": "11.12",
+        "interestedCount": 6,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "A Tribute to Hans Zimmer & John Williams by Moonlight in Leeds, Late Show",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F317184589%2F61565826027%2F1%2Foriginal.20220512-141427?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2480%2C1240&s=7ec41fc3daed5cbdcf9e457fd3b20112",
+        "description": "Experience cinema's most iconic music performed by The London Film Music Chamber Orchestra in a moon-lit setting.",
+        "location": "City Varieties Music Hall, Leeds",
+        "date": "2022-10-30T00:00:00.000Z",
+        "price": "34.74",
+        "interestedCount": 100,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "The Rock Orchestra by Candlelight: Leeds",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F362796519%2F61565826027%2F1%2Foriginal.20220928-105517?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C47%2C2048%2C1024&s=e1fbd7de043101e093dda98880bfb860",
+        "description": "A chamber orchestra breathing beautifully dark energy into legendary Rock & Metal tracks, in immersive settings.\n\nBathed in a sea of candles, skeletal players with rhinestone masks effortlessly switch between sublime melodies & powerful walls of sound.\n\nCrooked lurching towers, floating lanterns & giant head-banging puppets set the stage for an otherworldly musical experience.",
+        "location": "O2 Academy Leeds",
+        "date": "2023-02-05T00:00:00.000Z",
+        "price": "39.95",
+        "interestedCount": 9,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "Harrogate Library Lego Club",
+        "image": "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F369601159%2F442197439018%2F1%2Foriginal.20220612-073532?w=940&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C27%2C1594%2C797&s=2ecda58db69b952acbe6f4647f09a7db",
+        "description": "Suitable for ages 4+. Challenges will be adapted according to age.\n\nPlease book ONE place for each participating child.",
+        "location": "Harrogate Library",
+        "date": "2022-10-15T00:00:00.000Z",
+        "price": "0",
+        "interestedCount": 93,
+        "interested": false,
+        "tags": []
+    },
+    {
+        "title": "Example empty event",
+        "image": "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+        "description": "A description",
+        "location": "Some location",
+        "date": "2022-10-14T00:00:00.000Z",
+        "price": "0",
+        "interestedCount": 96,
+        "interested": false,
+        "tags": []
+    }
+]
+
+for (let event of EVENTS) {
+    event.date = new Date(event.date)
+}
+
+const eventsSection = document.getElementById("events")
+
+function getInterestedIcon(interested) {
+    return interested ? "fi-br-check" : "fi-ss-bell"
+}
+
+function createNewEvent(eventData) {
+    const event = document.createElement("article")
+    event.classList.add("event")
+
+    const date = eventData.date
+    event.innerHTML = `<section class="numbers">
+        <span class="icon-info"
+            ><i class="fi fi-rr-calendar"></i
+            ><span>${date.toDateString()}</span></span
+        >
+        <span class="interested-count"
+            ><i class="fi fi-rr-user"></i><span>${eventData.interestedCount}</span></span
+        >
+    </section>
+    <section class="space">
+        <section class="middle">
+            <h2>${eventData.title}</h2>
+            <div
+                class="image"
+                style="
+                    --image: url(${eventData.image});
+                "
+            ></div>
+            <span class="icon-info"
+                ><i class="fi fi-rs-marker"></i
+                ><span>${eventData.location}</span></span
+            >
+            <p>${eventData.description}</p>
+        </section>
+        <section class="buttons">
+            <div class="price"><h3>${eventData.price > 0 ? (new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' })).format(eventData.price) : "Free"}</h3></div>
+            <button class="interested">
+                <i class="fi ${getInterestedIcon(eventData.interested)}"></i>
+                <span>Interested</span>
+            </button>
+        </section>
+    </section>
+    `
+
+    const interestedCount = event.getElementsByClassName("interested-count")[0].querySelector("span")
+
+    const interestedButton = event.getElementsByClassName("interested")[0]
+    const interestedIcon = interestedButton.querySelector("i")
+
+    interestedButton.addEventListener("click", () => {
+        let interested = !eventData.interested
+        eventData.interested = interested
+
+        interestedIcon.classList.remove(getInterestedIcon(!interested))
+        interestedIcon.classList.add(getInterestedIcon(interested))
+
+        if (interested) {
+            interestedButton.classList.add("is-interested")
+            eventData.interestedCount++
+        } else {
+            interestedButton.classList.remove("is-interested")
+            eventData.interestedCount--
+        }
+        interestedCount.innerText = eventData.interestedCount
+    })
+
+    eventsSection.append(event)
+}
+
+//
+function reloadEvents(filter) {
+    eventsSection.replaceChildren()
+    let eventsFound = 0
+
+    for (let eventData of EVENTS) {
+        if (filter) {
+            if (filter.search && !eventData.title.toLowerCase().includes(filter.search.toLowerCase())) {
+                continue
+            }
+        }
+        createNewEvent(eventData)
+        eventsFound++
+    }
+
+    if (eventsFound == 0) {
+        eventsSection.innerHTML = "<h2>No events found!</h2>"
+    }
+}
+
+const searchBar = document.getElementById("search")
+searchBar.addEventListener("input", () => {
+    let value = searchBar.value
+    if (value) {
+        reloadEvents({
+            search: value
+        })
+    } else {
+        reloadEvents()
+    }
+})
+//
+
+const modalContainer = document.getElementById("modal-container")
+
+let currentModal;
+function closeModal() {
+    if (currentModal) {
+        modalContainer.classList.add("invis")
+        currentModal.classList.add("invis")
+
+        const closeButton = currentModal.getElementsByClassName("cancel")[0]
+        closeButton.removeEventListener("click", closeModal)
+        currentModal = null
+    }
+}
+
+function openModal(modal) {
+    closeModal()
+
+    currentModal = modal
+
+    const closeButton = currentModal.getElementsByClassName("cancel")[0]
+    closeButton.addEventListener("click", closeModal)
+
+    modal.classList.remove("invis")
+    modalContainer.classList.remove("invis")
+}
+
+const createButton = document.getElementById("create")
+createButton.addEventListener("click", () => {
+    openModal(document.getElementById("create-modal"))
+})
+
+const newEventForm = document.getElementById("new-event-form")
+const submitEventButton = document.getElementById("submit-event")
+submitEventButton.addEventListener("click", () => {
+    const data = new FormData(newEventForm)
+
+    if (!(data.get("title") && data.get("description") && data.get("date") && data.get("location"))) {
+        return
+    }
+
+    const eventData = {
+        title: data.get("title"),
+        image: data.get("image") ? data.get("image") : "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+        description: data.get("description"),
+        location: data.get("location"),
+        date: new Date(data.get("date")),
+        price: data.get("price"),
+        interestedCount: 0,
+        interested: false,
+        tags: data.get("tags").split(",").reduce((tags, tag) => {
+            tags.push(tag.trim())
+            return tags
+        }, [])
+    }
+
+    EVENTS.push(eventData)
+    // console.log(EVENTS)
+
+    createNewEvent(eventData)
+
+    closeModal()
+    newEventForm.reset()
+})
+
+reloadEvents()
